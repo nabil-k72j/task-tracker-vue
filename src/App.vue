16402936c7b6b311ca37781bc821e7db5c5a6f94
nabil-k72j <nabilmalki400@gmail.com>
2023-11-10 13:41:22 +0100

    < task-tracker >

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+<Header />
+<div class="container">
+  <Balance :total="+total"/>
+  <IncomeExpense :income="+income" :expenses="+expenses"/>
+  <Transactions :transactions="transactions" @transactionDeleted="handleDelete"/>
+  <NewTransaction @transactionSubmitted="handleTransaction"/>
+</div>
+</template>
+
+
+<script setup>
+
+  import Header from './components/Header.vue'
+  import Balance from './components/Balance.vue';
+  import IncomeExpense from './components/IncomeExpense.vue';
+  import Transactions from './components/Transactions.vue';
+  import NewTransaction from './components/NewTransaction.vue';
+  import { useToast } from 'vue-toastification';
+  import { ref, computed, onMounted } from 'vue';
+
+
+  const toast = useToast()
+
+  const transactions = ref([])
+
+  onMounted(() => {
+    const savedTransactions = JSON.parse(localStorage.getItem("transactions"))
+
+    if(savedTransactions){
+      transactions.value = savedTransactions
+    }
+  })
+
+
+//Get Total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0)
+})
+
+//Get Income
+const income = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount > 0)
+  .reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0).toFixed(2)
+})
+
+
+//Get Expense
+const expenses = computed(() => {
+  return transactions.value
+  .filter((transaction) => transaction.amount < 0)
+  .reduce((acc, transaction) => {
+    return acc + transaction.amount
+  }, 0).toFixed(2)
+})
+
+
+//generate Unique Id
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000)
+}
+
+//Add transaction
+const handleTransaction= (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount
+  })
+
+  saveTransactions()
+
+  toast.success("New item Added")
+}
+
+
+//Delete transaction
+const handleDelete = (id) => {
+  transactions.value = transactions.value.filter(transaction => transaction.id !== id)
+
+  saveTransactions()
+
+  toast.success("Item deleted")
+}
+
+
+//Save to localStorage
+const saveTransactions = () => {
+  localStorage.setItem('transactions', JSON.stringify(transactions.value))
+}
+
+</script>
---
+++ src/components/Transactions.vue
@@ -0,0 +1,61 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>${{ transaction.amount }}</span>
+            <button @click="() => deleteTransaction(transaction.id)" class="delete-btn">X</button>
+        </li>
+    </ul>
+</template>
+
+<script setup>
+    import { defineProps } from 'vue';
+
+    const emit = defineEmits(["transactionDeleted"])
+
+    const props = defineProps({
+        transactions: {
+            type: Array,
+            required: true
+        }
+    })
+
+    const deleteTransaction = (id) => {
+        emit('transactionDeleted', id)
+    }
+</script>
+
+
+
+
+<!-- 
+Composition Api
+export default {
+    setup() {
+        const transactions = [
+                {id: 1, text: "Flower", amount: -19.99},
+                {id: 2, text: "Salary", amount: 299.97},
+                {id: 3, text: "Book", amount: -10},
+                {id: 4, text: "Camera", amount: 150},
+            ]
+
+        return {
+            transactions
+        }
+    }
+} -->
+
+
+<!-- Options Api
+export default {
+    data() {
+        return {
+            transactions: [
+                {id: 1, text: "Flower", amount: -19.99},
+                {id: 2, text: "Salary", amount: 299.97},
+                {id: 3, text: "Book", amount: -10},
+                {id: 4, text: "Camera", amount: 150},
+            ]
+        }
+    }
+} -->
